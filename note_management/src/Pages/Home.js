de165fed5b0c037c6bd70a099624ae757f24728e
06chirag06 <cchauhan0040@gmail.com>
2024-01-03 23:15:32 +0530

    Commit1: Login and Signup

---
+++ note_management/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeNavbar from "../Components/HomeNavbar";
+
+export default function Home() {
+  return (
+    <>
+      <HomeNavbar />
+    </>
+  );
+}
---
+++ note_management/src/Pages/Signup.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SignupForm from "../Components/SignupForm";
+import SignUpNavbar from "../Components/SignUpNavbar";
+
+export default function Signup() {
+  return (
+    <>
+      <SignUpNavbar />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="offset-4 col-5">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

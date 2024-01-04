967f83c6ec5b0ded0a3fd8f88a55ad324a35d833
Chetan Barmhanwade <chetanbramhanwade@gmail.com>
2024-01-04 12:39:38 +0530

    Trying working branch change

--- note_management/src/Pages/Notes.js
+++ note_management/src/Pages/Notes.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Users() {
   return (
     <>
-    <div>This is Notes page</div>
       <div>Add a SideBar</div>
       <div>
         Add a button to add notes on the bottom-right corner of the page

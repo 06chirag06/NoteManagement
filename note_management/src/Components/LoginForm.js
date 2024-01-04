ddebe983fcff694b6096a1089edc1b2a72ba9493
06chirag06 <cchauhan0040@gmail.com>
2024-01-04 16:32:51 +0530

    Commit final;

--- note_management/src/Components/LoginForm.js
+++ note_management/src/Components/LoginForm.js
@@ -1,49 +1,44 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import UserPass from "./UserPass";
 
 export default function LoginForm() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    userInfo: {},
+  });
 
-  const handleInput = (e) => {
-    switch (e.target.id) {
-      case "username":
-        setUsername(e.target.value);
-        break;
-      case "password":
-        setPassword(e.target.value);
-        break;
-      default:
-        break;
-    }
+  const handleUserPassChange = (userInfo) => {
+    setFormData({ ...formData, userInfo });
+    console.log(formData.userInfo);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const reqBody = {
-      username: username,
-      password: password,
-    };
+    // const reqBody = {
+    //   username: formData.userInfo,
+    //   password: formData.userInfo,
+    // };
 
-    try {
-      const response = await axios.post("http://192.168.182.33:8000/login", reqBody);
-      if(response.data.token){
-        localStorage.setItem("token", response.data.token);
-        navigate("/userID/home", {replace:true});
-      }
-      else{
-        const err = {Error:"Invalid Credentials"};
-        throw err;
-      }
-    } catch (err) {
-      console.log(err);
-      alert("Invalid Credentials");
-      setUsername("");
-      setPassword("");
-    }
+    // try {
+    //   const response = await axios.post(
+    //     "http://192.168.182.33:8000/login",
+    //     reqBody
+    //   );
+    //   if (response.data.token) {
+    //     localStorage.setItem("token", response.data.token);
+    //     navigate("/userID/home", { replace: true });
+    //   } else {
+    //     const err = { Error: "Invalid Credentials" };
+    //     throw err;
+    //   }
+    // } catch (err) {
+    //   console.log(err);
+    //   alert("Invalid Credentials");
+    //   // setFormData({ ...formData, {} });
+    // }
+    navigate("/userId/home", { replace: true });
   };
 
   const handleSignUp = (e) => {
@@ -53,32 +48,7 @@ export default function LoginForm() {
   return (
     <form onSubmit={handleSubmit}>
       <div className="offset-5 fw-bold fs-2">Login</div>
-      <div>
-        <label htmlFor="username" className="form-label mt-2">
-          Username
-        </label>
-        <input
-          className="form-control"
-          type="text"
-          id="username"
-          name="username"
-          onChange={handleInput}
-          value={username}
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="form-label mt-2">
-          Password
-        </label>
-        <input
-          className="form-control"
-          type="password"
-          id="password"
-          name="password"
-          onChange={handleInput}
-          value={password}
-        />
-      </div>
+      <UserPass onChange={handleUserPassChange} />
       <div>
         <button type="submit" className="btn btn-primary mt-2 offset-5 mb-2">
           Login

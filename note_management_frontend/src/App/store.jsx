129f2aa8f8282016e8fad8e1682efdd19432c066
Chirag Chauhan <68694166+06chirag06@users.noreply.github.com>
2024-02-25 00:24:46 +0530

    Update store.jsx

--- note_management_frontend/src/App/store.jsx
+++ note_management_frontend/src/App/store.jsx
@@ -1,6 +1,3 @@
-import { configureStore, createStore } from "@reduxjs/toolkit";
-import usernameReducer from "./reducers/usernameSlice";
-import { createStoreHook } from "react-redux";
 import rootReducer from "./rootReducer";
 
 const store = createStore(rootReducer);
